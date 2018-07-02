200c69863ea15074cc5fdf1c258dc82b1164395b
Anderson Meira <anderson.meira@gmail.com>
2018-07-02 13:09:09 -0300

    remove useless code

--- app.js
+++ app.js
@@ -1,34 +1,23 @@
 var express = require('express');
-var path = require('path');
-//var favicon = require('serve-favicon');
+
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-//var responseTime = require('response-time');
+
 var compression = require('compression');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 var links = require('./routes/links');
 
 var app = express();
 
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'hbs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
-//app.use(responseTime());
 app.use(compression());
 
 app.use('/', routes);
-app.use('/users', users);
 app.use('/api/links',links);
 
 // catch 404 and forward to error handler
